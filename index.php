f1f98ae1a586e05081abad2ab1319fb810da24ac
Satria <satadii11@gmail.com>
2018-07-30 00:10:12 +0700

    change title Tambah Petugas -> Beranda

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 include_once('app/core/init.php');
 include_once('app/resources/menu.php');
 
-$title = 'Tambah Petugas - Perpustakaan';
+$title = 'Beranda - Perpustakaan';
 $navMenu = getNavbarMenu($_SESSION['userJenis']);
 $navSubMenu = getNavbarSubMenu($_SESSION['userJenis']);
 
